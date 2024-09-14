0f8eda7ff1e4719b857699065fc0ce80fe685a13
Nadia Islam <sohasara853@gmail.com>
2024-09-14 11:26:59 +0600

    modify

--- lib/fixedOverflow.dart
+++ lib/fixedOverflow.dart
@@ -1,2 +1,66 @@
 // ignore_for_file: file_names
 
+import 'package:container_overflow/text.dart';
+import 'package:flutter/material.dart';
+
+class FixedOverflow extends StatelessWidget {
+  const FixedOverflow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.pink,
+        title: const Text(
+          'Container OverFlow',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 400,
+              width: 400,
+              decoration: BoxDecoration(
+                color: Colors.pink[300],
+                borderRadius: BorderRadius.circular(18),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 200,
+                    width: 200,
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                    child: const SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Textt(),
+                          Textt(),
+                          Textt(),
+                          Textt(),
+                          Textt(),
+                          Textt(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:container_overflow/fixedOverflow.dart';
 import 'package:container_overflow/overflow.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +14,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.light(),
       debugShowCheckedModeBanner: false,
-      home: const OverFlow(),
+      //home: const OverFlow(),
+      home: const FixedOverflow(),
     );
   }
 }
