247bc98d66fffa61b4534129f4c89871833a61d3
Nadia Islam <sohasara853@gmail.com>
2024-09-18 19:52:42 +0600

    add container overflow

--- lib/overflow.dart
+++ lib/overflow.dart
@@ -32,17 +32,26 @@ class OverFlow extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
-                    height: 200,
-                    width: 200,
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(18),
-                    ),
-                    child: const Column(
-                      children: [],
-                    ),
-                  ),
+                      height: 200,
+                      width: 200,
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 210,
+                            width: 150,
+                            decoration: BoxDecoration(
+                              color: Colors.pink,
+                              borderRadius: BorderRadius.circular(18),
+                            ),
+                          ),
+                        ],
+                      )),
                 ],
               ),
             ),
